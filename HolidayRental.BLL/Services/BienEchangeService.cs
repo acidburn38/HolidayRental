4888d99145073dc176dc5fa870d3760a80a63466
Laure <lacomtessebathory@gmail.com>
2022-02-22 16:34:57 +0100

    Add Service "BienEchange" to BLL

--- HolidayRental.BLL/Services/BienEchangeService.cs
+++ HolidayRental.BLL/Services/BienEchangeService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HolidayRental.BLL.Entities;
+using HolidayRental.BLL.Handlers;
+using HolidayRental.Common.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,37 @@ using System.Threading.Tasks;
 
 namespace HolidayRental.BLL.Services
 {
-    public class BienEchangeService
+    public class BienEchangeService : IBienEchangeRepository<BienEchange>
     {
+        private readonly IBienEchangeRepository<DAL.Entities.BienEchange> _bienEchangeRepository;
+
+        public BienEchangeService(IBienEchangeRepository<DAL.Entities.BienEchange> repository)
+        {
+            _bienEchangeRepository = repository;
+        }
+        public void Delete(int idBien)
+        {
+            _bienEchangeRepository.Delete(idBien);
+        }
+
+        public BienEchange Get(int idBien)
+        {
+            return _bienEchangeRepository.Get(idBien).ToBLL();
+        }
+
+        public IEnumerable<BienEchange> Get()
+        {
+            return _bienEchangeRepository.Get().Select(c => c.ToBLL());
+        }
+
+        public int Insert(BienEchange entity)
+        {
+            return _bienEchangeRepository.Insert(entity.ToDAL());
+        }
+
+        public void Update(int idBien, BienEchange entity)
+        {
+            _bienEchangeRepository.Update(idBien, entity.ToDAL());
+        }
     }
 }
